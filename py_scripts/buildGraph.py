8ffdd445ca6f551ad6d8bcc1247d16a6948bb53c
JoetheManHowie <joehowie@protonmail.com>
2021-04-10 23:22:18 -0700

    now the graph to edge list does the edge distribution that are done the the coarsen paper

---
+++ py_scripts/buildGraph.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+from numpy.random import randint
+from numpy import empty, vstack, array
+from time import time
+import sys
+import os
+
+def main():
+    basename = make_txt_file()
+    print("text file made")
+    run_java_macros(basename)
+
+
+def run_java_macros(basename):
+    os.system("sort -k1n -k2n "+basename+".txt | uniq > "+basename+"_sorted.txt")
+    print("sorted")
+    os.system("java -cp \"../lib/*\" it.unimi.dsi.webgraph.BVGraph -1 -g ArcListASCIIGraph dummy "+basename+" < "+basename+"_sorted.txt")
+    os.system("java -cp \"../bin\":\"../lib/*\" GenerateLabeledGraphFromTxt "+basename+" "+basename+"_sorted.txt")
+    os.system("java -cp \"../bin\":\"../lib/*\" PrintLabeledGraph "+basename)
+    
+
+def make_txt_file():
+    nodes = int(sys.argv[1])
+    edges = int(sys.argv[2])
+    added = empty((1,2))
+    basename ="graph_"+str(nodes)+"_"+str(edges) 
+    f = open(basename+".txt", "w+")
+    e_count = 0
+    for u in range(nodes):
+        v = randint(nodes)
+        vstack((added, array([u, v])))
+        weight = randint(1, 1000) # range [1, 1000)
+        string = "%d\t%d\t%d\n"%(u,v,weight)
+        f.write(string)
+        e_count+=1
+        #add_edge(f, u, e_count, nodes, added)
+    while e_count<edges:
+        u = randint(nodes)
+        v = randint(nodes)
+        vstack((added, array([u, v])))
+        weight = randint(1, 1000) # range [1, 1000)
+        string = "%d\t%d\t%d\n"%(u,v,weight)
+        f.write(string)
+        e_count+=1
+        #add_edge(f, u, e_count, nodes, added)
+    f.close()
+    return basename
+
+
+if __name__=="__main__":
+    t1 = time()
+    main()
+    print(time()-t1)
+
+
+
+
+'''     
+        for e in range(edges):
+        u = randint(nodes)
+        v = randint(nodes)
+        ## no self loops or repeat edges
+        while (u == v or (u, v) in added):
+            u = randint(nodes)
+            v = randint(nodes)
+        vstack((added, array([u, v])))
+        #added.append((u, v))
+        #print(u, v)
+        weight = randint(1, 1000) # range [1, 1000)
+        string = "%d\t%d\t%d\n"%(u,v,weight)
+'''
---
+++ src/GraphToEdgelist.java
@@ -0,0 +1,101 @@
+/*
+ * Joe Howie: April 10th 2021
+ */
+//package grToEdge;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntArrays;
+import it.unimi.dsi.fastutil.ints.IntIterator;
+import it.unimi.dsi.fastutil.objects.ObjectArrays;
+import it.unimi.dsi.lang.MutableString;
+import java.util.*;
+import com.google.common.collect.Sets;
+import it.unimi.dsi.webgraph.ImmutableGraph;
+import it.unimi.dsi.webgraph.NodeIterator;
+import it.unimi.dsi.webgraph.ImmutableSequentialGraph;
+import java.io.*;
+
+
+public class GraphToEdgelist{
+    ImmutableGraph g;
+    int n;
+    String basename;
+    public static <S> void print(S s){
+	System.out.println(s);
+    }
+    public GraphToEdgelist(String basename)throws Exception{
+	print("Load Graph...");
+	g = ImmutableGraph.loadMapped("webgraph/"+basename);
+	n = g.numNodes();
+	this.basename = basename;
+    }
+    public void uniform()throws Exception{
+	print("uniform chosen");
+	PrintWriter uni_writer = new PrintWriter("graphs/"+basename + "_uniform.edgelist");;
+	for (int i = 0 ; i < n ; i++){
+	    for (int neigh: g.successorArray(i)){
+		int x = 100; // 0.1
+		uni_writer.println(i + "\t" + neigh + "\t" + x );
+	    }
+	}
+	uni_writer.flush();
+    }
+    public void trivalency()throws Exception{ // unifom from set {0.1, 0.01, 0.001}
+	print("trivalency chosen");
+	Random tri_rnd = new Random();
+	PrintWriter tri_writer = new PrintWriter("graphs/"+basename + "_trivalency.edgelist");;;
+	for (int i = 0 ; i < n ; i++){
+	    for (int neigh: g.successorArray(i)){
+		int y = tri_rnd.nextInt(3);
+		int x;
+		if(y == 1) x = 100;
+		else if(y == 2) x = 10;
+		else x = 1;
+		tri_writer.println(i + "\t" + neigh + "\t" + x );
+	    }
+	    tri_writer.flush();
+	}
+    }
+    public void exponential()throws Exception{
+	print("exponential chosen");
+	Random exp_rnd = new Random();
+	PrintWriter exp_writer = new PrintWriter("graphs/"+basename + "_exponential.edgelist");;;
+	int lambda = 10; // mean =  1 / (lambda) = 0.1 in the paper
+	int scaling = 100;
+	for (int i = 0 ; i < n ; i++){
+	    for (int neigh: g.successorArray(i)){ 
+		double y = exp_rnd.nextInt(70)/100.0; // for D = [0, 0.69], R = [1, 1000]
+		int x = (int)Math.round((scaling*lambda * Math.exp(-y * lambda)));
+		exp_writer.println(i + "\t" + neigh + "\t" + x );
+	    }
+	}
+	exp_writer.flush();
+    }
+    public void weighted()throws Exception{
+	print("weighted chosen");
+	PrintWriter wei_writer = new PrintWriter("graphs/"+basename + "_weighted.edgelist");;;
+	for (int i = 0 ; i < n ; i++){
+	    for (int neigh: g.successorArray(i)){
+		int x = 10;
+		wei_writer.println(i + "\t" + neigh + "\t" + x );
+	    }
+	}
+	wei_writer.flush();
+    }
+    public static void main(String[] args)throws Exception{
+	long t1 = System.currentTimeMillis();
+	String basename = args[0];
+	String distro_flag = args[1]; // 0 = uniform (all 10), 1 = {100, 10, 1}, 2 = exponential, 3 = weighted (d_in) 
+	GraphToEdgelist edgelist = new GraphToEdgelist(basename);
+	switch(distro_flag){
+	case "0": edgelist.uniform();
+	    break;
+	case "1": edgelist.trivalency();
+	    break;
+	case "2": edgelist.exponential();
+	    break;
+	case "3": edgelist.weighted();
+	default: break; 
+	}
+	print("Done! "+(System.currentTimeMillis()-t1)/1000.0+" seconds");
+    }
+}
