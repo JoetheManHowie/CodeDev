e917e82c5c90d64a0758bd8a94b75a1022d98f07
JoetheManHowie <joehowie@protonmail.com>
2021-05-19 19:26:25 -0700

    updating bash scripts

--- genPG.sh
+++ genPG.sh
@@ -4,15 +4,14 @@ pat1=.obl
 web_path=./webgraph/*$pat1
 pat2=.edgelist
 type='trivalency exponential'
-day=$(date +%F)
+day=$(date +%F_%T)
 for graph in $web_path
 do
     name=$(basename $graph $pat1)
     echo $name
     java -cp "bin":"lib/*" GraphToEdgelist $name
-    
 done
-echo done first loop
+echo ------- done first loop
 cd graphs/
 ls *$pat2
 for ext in $type
@@ -21,5 +20,11 @@ do
     do
 	name=$(basename $graph \_$ext$pat2)
 	echo this $name
+	saved_name=$name\_$day\_$ext
+	echo $saved_name
+	sort -k1n -k2n $name\_$ext.edgelist | uniq > $saved_name.sorted
+	rm $name\_$ext.edgelist
+	java -cp "../lib/*" it.unimi.dsi.webgraph.BVGraph -1 -g ArcListASCIIGraph dummy $saved_name < $saved_name.sorted
+	java -cp "../bin":"../lib/*" GenerateLabeledGraphFromTxt $saved_name $saved_name.sorted
     done
 done
